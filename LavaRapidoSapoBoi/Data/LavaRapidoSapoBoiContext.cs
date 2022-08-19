e65aa2d4a0c0e2bbe55813d71a6da556748c5e2b
Asilva-01 <andre1981luiz@gmail.com>
2022-08-18 22:26:39 -0300

    [200~Deleting Department view and controller

---
+++ LavaRapidoSapoBoi/Data/LavaRapidoSapoBoiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LavaRapidoSapoBoi.Models
+{
+    public class LavaRapidoSapoBoiContext : DbContext
+    {
+        public LavaRapidoSapoBoiContext (DbContextOptions<LavaRapidoSapoBoiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LavaRapidoSapoBoi.Models.Departamento> Departamento { get; set; }
+    }
+}
--- LavaRapidoSapoBoi/Startup.cs
+++ LavaRapidoSapoBoi/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LavaRapidoSapoBoi.Models;
 
 namespace LavaRapidoSapoBoi
 {
@@ -33,6 +35,9 @@ namespace LavaRapidoSapoBoi
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<LavaRapidoSapoBoiContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("LavaRapidoSapoBoiContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

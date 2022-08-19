f1680c823da507cd83b74e95766a401b13fd4005
Asilva-01 <andre1981luiz@gmail.com>
2022-08-18 22:59:16 -0300

    MSQL adaptação e primeira migração

--- LavaRapidoSapoBoi/Startup.cs
+++ LavaRapidoSapoBoi/Startup.cs
@@ -37,7 +37,8 @@ namespace LavaRapidoSapoBoi
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<LavaRapidoSapoBoiContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("LavaRapidoSapoBoiContext")));
+                    options.UseMySql(Configuration.GetConnectionString("LavaRapidoSapoBoiContext"), builder =>
+                        builder.MigrationsAssembly("LavaRapidoSapoBoi")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
